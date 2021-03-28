2a529ee92063b75f4bff83cf476cc2f971918b35
Blackwell  Alexander G <blackwellag>
2021-03-28 17:05:37 -0400

    Database is done

--- Main.java
+++ Main.java
@@ -12,28 +12,19 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello There, Welcome to the Mineral Database.");
-        System.out.println("Enter any characteristic file of a mineral based on the following:\n\tName\n\tCrystal Symetry\n\tHardness\n\tLuster\n\tColor\n\tCleavage");
-        System.out.println("Any mineral that fits into the characteristic given will show up as a possible option.\n");
-        System.out.println("Enter in file you want to use.");
+        System.out.println("Here you can search the Database by any category.");
+        System.out.println("If you sort by luster or color,\n\tdata will be duplicated to for each component it has listed");
+        System.out.println("\n-----------------------------------\n");
 
-        Scanner scanner = new Scanner(System.in);
-        String fileName = "";
-        File f1;
-
-        // Make sure a valid filename was entered
-        do {
-//            fileName = scanner.nextLine();
-fileName = "Minerals.txt";
-            f1 = new File(fileName);
-            if (!f1.exists())
-                System.out.println("Could not find file, try another");
-        } while (!f1.exists());
+        String fileName = "Minerals.txt";
 
         ArrayList<Mineral> mineralList = readFile(fileName);
-        
         Sorter workingSet = options(mineralList);
 
         toFile(workingSet);
+
+        System.out.println("The data set is sorted based off your input.\n---");
+        System.out.println("Have a gneiss day!\n");
     }
 
     public static Sorter options(ArrayList<Mineral> mineralList)
@@ -42,73 +33,28 @@ fileName = "Minerals.txt";
         String answer = "";
         int ans = -1;
         boolean b;
-
-        do {
-        System.out.println("Do you want to sort the data? (y/n)");
-        answer = scanner.nextLine().toLowerCase();
-        } while (answer.compareTo("y") == 0 && answer.compareTo("n") == 0);
         
-        if (answer.compareTo("y") == 0)
+        System.out.println("Please select a trait to sort by:");
+        System.out.println("1. Sort by Name");
+        System.out.println("2. Sort by Crystal Structure");
+        System.out.println("3. Sort by Hardness");
+        System.out.println("4. Sort by Luster");
+        System.out.println("5. Sort by Color");
+        System.out.println("6. Sort by Cleavage");
+
+
+        do
         {
-            do {
-            System.out.println("Please select a trait to sort by:");
-            System.out.println("1. sort by name");
-            System.out.println("2. sort by crystal structure");
-            System.out.println("3. sort by hardness");
-            System.out.println("4. sort by luster");
-            System.out.println("5. sort by color");
-            System.out.println("6. sort by cleavage");
-
-
             ans = scanner.nextInt();
             if (ans < 1 || ans > 6)
                 System.out.println("Please enter a number 1-6");
-            } while (ans < 1 || ans > 6);
+        } while (ans < 1 || ans > 6);
 
-            //sort by ans
-            Sorter temp = new Sorter(mineralList);
-            temp.setSortKey(--ans);
+        //sort by ans
+        Sorter temp = new Sorter(mineralList);
+        temp.setSortKey(--ans);
 
-            return temp;
-        }
-        else
-        {
-            System.out.println("Have a gneiss day!");
-            return null;
-        }
-    }
-
-    public static void addToSet()
-    {
-        String answer;
-        boolean b;
-        Scanner scanner = new Scanner(System.in);
-        
-        do {
-            System.out.println("Choose one of the following catagories to add to the data set :");
-            System.out.println("\t1. Name\n\t2. Crystal System\n\t3. Hardness\n\t4. Luster\n\t5. Color\n\t6. Cleavage\n");
-
-            answer = scanner.nextLine().toLowerCase().replaceAll("\\s","");
-            
-            b = false;
-            b |= (answer.compareTo("1") == 0);
-            b |= (answer.compareTo("name") == 0);
-            b |= (answer.compareTo("2") == 0);
-            b |= (answer.compareTo("crystalsystem") == 0);
-            b |= (answer.compareTo("3") == 0);
-            b |= (answer.compareTo("hardness") == 0);
-            b |= (answer.compareTo("4") == 0);
-            b |= (answer.compareTo("luster") == 0);
-            b |= (answer.compareTo("5") == 0);
-            b |= (answer.compareTo("color") == 0);
-            b |= (answer.compareTo("6") == 0);
-            b |= (answer.compareTo("cleavage") == 0);
-
-            if (!b)
-            {
-                System.out.println("Please enter 1-6 or the catagory's name");
-            }
-        } while (!b);
+        return temp;
     }
 
     public static ArrayList<Mineral> readFile(String fileName)
@@ -208,6 +154,7 @@ fileName = "Minerals.txt";
         return null;
     }
 
+
     public static void toFile(Sorter sorted)
     {
         File myObj = new File("dump.txt");
@@ -216,92 +163,10 @@ fileName = "Minerals.txt";
             FileWriter myWriter = new FileWriter("dump.txt");
             myWriter.write(sorted.toString());
             myWriter.close();
-            System.out.println("Successfully wrote to data.dump.");
+            System.out.println("\nSuccessfully wrote to data.dump.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 }
-
-
-
-
-
-
-
-
-/*
-
-
-do {
-            // give the user the options they can do with the data set
-
-            System.out.println("Here are the following options or enter 'back' to return:");
-            b = false;
-
-            // If there is no set yet
-            if (workingSet.isEmpty())
-            {
-                // option 1
-                System.out.println("\t1. Start new data set");
-                answer = scanner.nextLine().toLowerCase();
-
-                b |= (answer.compareTo("1") == 0);
-                b |= (answer.toLowerCase().compareTo("back") == 0);
-
-                if (answer.toLowerCase().compareTo("back") == 0)
-                    return null;
-                if (!b)
-                    System.out.println("Please enter 1 or 'back' to continue");
-                else
-                {
-                    ArrayList<MineralNode> nodeList = new ArrayList<MineralNode>();
-
-                    for (int i = 0; i < mineralList.size(); i++)
-                        nodeList.add(i, new MineralNode(mineralList.get(i)));      // Minerals to Nodes
-
-                    return new Sorter(nodeList, mineralList);
-                }
-            }
-
-            // If a set is present ...
-            else if (!workingSet.isEmpty())
-            {
-                // option 2-4
-                System.out.println("\t1. Start new data set");
-                System.out.println("\t2. Print set");
-                System.out.println("\t3. Add to set");
-                System.out.println("\t4. Remove from set");
-                answer = scanner.nextLine().toLowerCase();
-
-                b |= (answer.toLowerCase().compareTo("back") == 0);
-                b |= (answer.compareTo("1") == 0);
-                b |= (answer.compareTo("2") == 0);
-                b |= (answer.compareTo("3") == 0);
-                b |= (answer.compareTo("4") == 0);
-
-                if (answer.toLowerCase().compareTo("back") == 0)
-                    return null;
-                else if (!b)
-                {
-                    //S.o.pl(enter 1);
-                    System.out.println("Please enter 1-4 or 'back' to continue");
-                }
-                else
-                {
-                    System.out.println("Cool... you entered 1-4");
-                }
-            }
-
-
-
-            // ask for input (which option)
-
-
-        } while (!b);
-        return null;
-
-
-
-        */
--- MineralNode.java
+++ MineralNode.java
@@ -255,4 +255,3 @@ public class MineralNode
 
         return -999;
     }
-}
